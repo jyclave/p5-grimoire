724ed18c9b9969b0bf3cad95171062ae25caf6a7
jyclave <jyclave@gmail.com>
2025-03-06 13:43:22 +0800

    uodate server

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log('Requête reçue !')
+  next()
+});
+
+
+app.use((req, res, next) => {
   res.json({ message: 'Votre requète a bien été reçue !'});
+  next();
 });
 
 module.exports = app;
