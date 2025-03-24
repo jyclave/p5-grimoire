d6fe609f98835df2aeac290cb142884cebd6183c
jyclave <jyclave@gmail.com>
2025-03-24 11:57:00 +0700

    jwt

--- BackEnd/middleware/auth.js
+++ BackEnd/middleware/auth.js
@@ -1,16 +1,39 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
+
+// module.exports = (req, res, next) => {
+//   try {
+//       const token = req.headers.authorization.split(' ')[1];
+//       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+//       const userId = decodedToken.userId;
+//       req.auth = {
+//           userId: userId
+//       };
+//    next();
+//   } catch(error) {
+//       res.status(401).json({ error });
+//   }
+// };
+
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-      const token = req.headers.authorization.split(' ')[1];
-      const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-      const userId = decodedToken.userId;
-      req.auth = {
-          userId: userId
-      };
-   next();
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: "Authentification requise" });
+    }
+    
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.userId;
+    
+    req.auth = {
+      userId: userId
+    };
+    
+    next();
   } catch(error) {
-      res.status(401).json({ error });
+    // Message d'erreur générique
+    res.status(401).json({ message: "Requête non authentifiée" });
   }
 };
