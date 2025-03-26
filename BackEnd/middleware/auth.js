c74d028c6af873db108848719e6a9fd495247a5b
jyclave <jyclave@gmail.com>
2025-03-26 09:02:13 +0700

    auth.js et user.js

--- BackEnd/middleware/auth.js
+++ BackEnd/middleware/auth.js
@@ -4,11 +4,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
       const token = req.headers.authorization.split(' ')[1];
-      const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-      const userId = decodedToken.userId;
-      req.auth = {
-          userId: userId
-      };
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      req.auth = { userId: decodedToken.userId };
    next();
   } catch(error) {
       res.status(401).json({ error });
